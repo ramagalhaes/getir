1f331b7b603ea0db1182f248a7d9a705384d8963
Raphael Magalhaes <raphael.gol@hotmail.com>
2022-04-07 17:01:43 -0300

    env adjustments

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+let baseURL = 'https://6239ffd028bcd99f0278782b.mockapi.io/api/v1/';
+
+if(process.env.NODE_ENV === 'development'){
+  baseURL = 'http://localhost:3004';
+}
+
+console.log(baseURL);
 const Api = axios.create({
-  baseURL: 'https://6239ffd028bcd99f0278782b.mockapi.io/api/v1/'
-  // baseURL: 'http://localhost:3004'
+  baseURL
 
 });
 
