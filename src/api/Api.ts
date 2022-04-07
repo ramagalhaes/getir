83652b19759a2b6262c346893637ae40bcbd16be
Raphael Magalhaes <raphael.gol@hotmail.com>
2022-04-07 16:48:36 -0300

    local API URL

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const Api = axios.create({
   baseURL: 'https://6239ffd028bcd99f0278782b.mockapi.io/api/v1/'
+  // baseURL: 'http://localhost:3004'
+
 });
 
 export default Api;
