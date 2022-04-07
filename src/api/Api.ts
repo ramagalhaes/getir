14f355c535fe8f9887703b73aa52f8e70dd3ebd8
Raphael Magalhaes <raphael.gol@hotmail.com>
2022-04-07 17:17:49 -0300

    url fix

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,15 +1,7 @@
 import axios from 'axios';
 
-let baseURL = 'https://6239ffd028bcd99f0278782b.mockapi.io/api/v1/';
-
-if(process.env.NODE_ENV === 'development'){
-  baseURL = 'http://localhost:3004';
-}
-
-console.log(baseURL);
 const Api = axios.create({
-  baseURL
-
+  baseURL: 'https://6239ffd028bcd99f0278782b.mockapi.io/api/v1/'
 });
 
 export default Api;
